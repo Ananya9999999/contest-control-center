8833bbf5c11e7e47bab8a81ec81c12732c04a05e
Ananya Singla <imananya07@gmail.com>
2026-06-28 13:03:50 +0530

    smthng

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import tsConfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: { port: 8080, host: "::" },
   plugins: [
-    tsConfigPaths({ projects: ["./tsconfig.json"] }),
-    tailwindcss(),
-    tanstackStart({ customViteReactPlugin: true }),
     react(),
+    tailwindcss(),
+    tsConfigPaths({ projects: ["./tsconfig.json"] }),
   ],
   resolve: {
     alias: { "@": "/src" },
